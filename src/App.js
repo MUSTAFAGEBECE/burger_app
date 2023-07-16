6e7696d69324bc3eadbe51736d202b52753f4fd9
MUSTAFAGEBECE <mustafagbc003@gmail.com>
2023-07-17 00:40:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
+import { Menu } from './components/Menu';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/menu" exact element={<Menu />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/contact" exact element={<Contact />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+
+/*  
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+<Routes>
+        <Route path="/" exact element={<Home />} />
+</Routes>
+import { Link } from 'react-router-dom';
+<Link to="/contact">İletişim</Link>
+<Link to="/menu">
+          <button>SİPARİŞ VER</button>
+</Link>
+*/
